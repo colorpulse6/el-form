9cb0dd2357d1bca8cb81e06cd374c3ec64dd6867
Tub <colorpulse@gmail.com>
2025-07-18 07:54:21 +0200

    fix: formatting

--- docs/tailwind.config.ts
+++ docs/tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: ["./src/**/*.{js,ts,jsx,tsx,md,mdx}", "./docs/**/*.{md,mdx}"],
-  darkMode: ['selector', '[data-theme="dark"]'],
+  darkMode: ["selector", '[data-theme="dark"]'],
   theme: {
     extend: {
       fontFamily: {
